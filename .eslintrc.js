1a2d18f75b500ceb4005db2ef46a7888752512bd
Csaba Palankai <csaba.palankai@gmail.com>
2022-09-04 10:41:37 +0100

    Adds linting

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/no-empty-function': 'off'
+  },
+  ignorePatterns: [".*.js", "*.config.js"]
+};
