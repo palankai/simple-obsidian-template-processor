6d781d9210ebe520800d9d6ce2c013bcaca8589b
Csaba Palankai <csaba.palankai@gmail.com>
2022-09-04 10:13:05 +0100

    Make it Prettier

--- tests/template.test.ts
+++ tests/template.test.ts
@@ -1,58 +1,57 @@
-import { TemplateProcessor } from "~/templateProcessor";
+import { TemplateProcessor } from '~/templateProcessor';
 
-describe("class TemplateProcessor", () => {
-  describe("with expected template string", () => {
-    const t = new TemplateProcessor("Hello {{name}}");
+describe('class TemplateProcessor', () => {
+  describe('with expected template string', () => {
+    const t = new TemplateProcessor('Hello {{name}}');
 
-    it("should render the substituted string", ()=> {
-      const result = t.render({name: "John"});
-      expect(result).toEqual("Hello John");
+    it('should render the substituted string', () => {
+      const result = t.render({ name: 'John' });
+      expect(result).toEqual('Hello John');
     });
   });
 
-  describe("with empty template", () => {
-    const t = new TemplateProcessor("");
+  describe('with empty template', () => {
+    const t = new TemplateProcessor('');
 
-    it("should render nothing", () => {
+    it('should render nothing', () => {
       const result = t.render();
-      expect(result).toEqual("");
+      expect(result).toEqual('');
     });
   });
 
-  describe("with some template but no params", () => {
-    const t = new TemplateProcessor("This is a string");
+  describe('with some template but no params', () => {
+    const t = new TemplateProcessor('This is a string');
 
-    it("should render the unmodified template", ()=> {
+    it('should render the unmodified template', () => {
       const result = t.render();
-      expect(result).toEqual("This is a string");
-    })
-  });
-
-  describe("with some template and whitespace around the variable", () => {
-    const t = new TemplateProcessor("Hello {{ name }}");
-
-    it("should render the substituted string", ()=> {
-      const result = t.render({name: "John"});
-      expect(result).toEqual("Hello John");
+      expect(result).toEqual('This is a string');
     });
   });
 
-  describe("with case non case matching params", () => {
-    const t = new TemplateProcessor("Hello {{ NAME }}");
+  describe('with some template and whitespace around the variable', () => {
+    const t = new TemplateProcessor('Hello {{ name }}');
 
-    it("should render the substituted string regardless of casing", ()=> {
-      const result = t.render({name: "John"});
-      expect(result).toEqual("Hello John");
+    it('should render the substituted string', () => {
+      const result = t.render({ name: 'John' });
+      expect(result).toEqual('Hello John');
     });
   });
 
-  describe("with function params with arguments", () => {
-    const t = new TemplateProcessor("Hello {{ name: John }}");
-    const result = t.render({name: (p: string) => p});
+  describe('with case non case matching params', () => {
+    const t = new TemplateProcessor('Hello {{ NAME }}');
 
-    it("should render the substituted string", ()=> {
-      expect(result).toEqual("Hello John");
+    it('should render the substituted string regardless of casing', () => {
+      const result = t.render({ name: 'John' });
+      expect(result).toEqual('Hello John');
     });
   });
 
+  describe('with function params with arguments', () => {
+    const t = new TemplateProcessor('Hello {{ name: John }}');
+    const result = t.render({ name: (p: string) => p });
+
+    it('should render the substituted string', () => {
+      expect(result).toEqual('Hello John');
+    });
+  });
 });
