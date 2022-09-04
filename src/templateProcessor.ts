34adf7120fca95cf8534c3a3fbf6c1d4a7515932
Csaba Palankai <csaba.palankai@gmail.com>
2022-09-04 12:28:58 +0100

    Make it feature complete

--- src/templateProcessor.ts
+++ src/templateProcessor.ts
@@ -1,22 +1,53 @@
+import moment, { Moment } from 'moment';
+
+export interface TemplateProcessorOpts {
+  _moment?: Moment;
+  dateFormat?: string;
+  timeFormat?: string;
+}
+
 export class TemplateProcessor {
   private templateString: string;
+  private _moment?: Moment;
+  private dateFormat: string;
+  private timeFormat: string;
 
-  constructor(templateString: string) {
+  constructor(templateString: string, opts?: TemplateProcessorOpts) {
     this.templateString = templateString;
+    this._moment = opts?._moment;
+    this.dateFormat = opts?.dateFormat || 'YYYY-MM-DD';
+    this.timeFormat = opts?.timeFormat || 'HH:mm';
+  }
+
+  moment(): Moment {
+    return this._moment || moment();
+  }
+
+  date(format?: string) {
+    return this.moment().format(format || this.dateFormat);
+  }
+
+  time(format?: string) {
+    return this.moment().format(format || this.timeFormat);
   }
 
   render(params?: Record<string, any>): string {
     let result = this.templateString;
-    if (params) {
-      for (const key in params) {
-        if (typeof params[key] == 'function') {
+    const replacers: Record<string, any> = {
+      date: (format?: string) => this.date(format),
+      time: (format?: string) => this.time(format),
+      ...params,
+    };
+    if (replacers) {
+      for (const key in replacers) {
+        if (typeof replacers[key] == 'function') {
           const re = new RegExp(`{{${key}(:(.*))?}}`, 'gi');
           result = result.replace(re, (_1, _2, param?: string) => {
-            return params[key](param ? param.trim() : param);
+            return replacers[key](param ? param.trim() : param);
           });
         } else {
           const re = new RegExp(`{{${key}}}`, 'gi');
-          result = result.replace(re, params[key]);
+          result = result.replace(re, replacers[key]);
         }
       }
     }
--- tests/templateProcessor.test.ts
+++ tests/templateProcessor.test.ts
@@ -1,11 +1,19 @@
-import { TemplateProcessor } from '~/templateProcessor';
+import moment from 'moment';
+import { TemplateProcessor, TemplateProcessorOpts } from '~/templateProcessor';
 
 interface SetupParams {
   template: string;
+  dateFormat?: string;
+  now?: string;
 }
 
 function setup(p: SetupParams) {
-  const tp = new TemplateProcessor(p.template);
+  const opts: TemplateProcessorOpts = {
+    _moment: p.now ? moment(p.now) : moment(),
+    dateFormat: p.dateFormat,
+  };
+
+  const tp = new TemplateProcessor(p.template, opts);
 
   return {
     tp,
@@ -58,4 +66,67 @@ test('With function params with arguments', () => {
   const result = tp.render({ name: (p: string) => p });
 
   expect(result).toEqual('Hello John');
+});
+
+test('With date substituted', () => {
+  const { tp } = setup({
+    template: 'Today is {{date}}',
+    now: '2022-08-03',
+  });
+
+  const result = tp.render();
+
+  expect(result).toEqual('Today is 2022-08-03');
+});
+
+test('With date substituted and default format specified', () => {
+  const tp = new TemplateProcessor('Today is {{date}}', {
+    _moment: moment('2022-08-03 12:34:56'),
+    dateFormat: 'DD/MM/YYYY',
+  });
+
+  const result = tp.render();
+
+  expect(result).toEqual('Today is 03/08/2022');
+});
+
+test('With date substituted and format specified in the argument', () => {
+  const tp = new TemplateProcessor('Today is {{date:DD/MM/YYYY}}', {
+    _moment: moment('2022-08-03 12:34:56'),
+  });
+  const result = tp.render();
+  expect(result).toEqual('Today is 03/08/2022');
+});
+
+test('With time substituted', () => {
+  const tp = new TemplateProcessor('Now is {{time}}', {
+    _moment: moment('2022-08-03 12:34:56'),
+  });
+  const result = tp.render();
+  expect(result).toEqual('Now is 12:34');
+});
+
+test('With time substituted and default format specified', () => {
+  const tp = new TemplateProcessor('Now is {{time}}', {
+    _moment: moment('2022-08-03 12:34:56'),
+    timeFormat: 'HH:mm:ss',
+  });
+  const result = tp.render();
+  expect(result).toEqual('Now is 12:34:56');
+});
+
+test('With time substituted and format specified in the argument', () => {
+  const tp = new TemplateProcessor('Now is {{time:HH:mm:ss}}', {
+    _moment: moment('2022-08-03 12:34:56'),
+  });
+  const result = tp.render();
+  expect(result).toEqual('Now is 12:34:56');
+});
+
+test('When a builtin function is overrode', () => {
+  const tp = new TemplateProcessor('Now is {{time:HH:mm:ss}}', {
+    _moment: moment('2022-08-03 12:34:56'),
+  });
+  const result = tp.render({ time: () => 'not a good time' });
+  expect(result).toEqual('Now is not a good time');
 });
